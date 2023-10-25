cbf404383c167ddc70b6083da716b618d1649b64
jixsu <jacksonxiao0@gmail.com>
2023-10-24 23:54:13 -0700

    add comment

--- protoshell-web/src/config.ts/env.ts
+++ protoshell-web/src/config.ts/env.ts
@@ -1,6 +1,8 @@
 import { parseEnv } from "znv";
 import { z } from "zod";
 
+// See https://vitejs.dev/guide/env-and-mode.html for info on adding env variables
+
 export const {
   VITE_SUPABASE_KEY: SUPABASE_KEY,
   VITE_SUPABASE_URL: SUPABASE_URL,

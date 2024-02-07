445490076fd18ec581e30c88a0a5275dee8e8098
repapt <samuelskliu@gmail.com>
2024-02-06 21:54:13 -0500

    added back button

--- protoshell-web/src/components/reusable/CompanyPage.tsx
+++ protoshell-web/src/components/reusable/CompanyPage.tsx
@@ -2,14 +2,17 @@ import { memo, useEffect, useState } from "react";
 // Ignore the ts error below, it is due to vite's client.d.ts shortcoming: https://github.com/vitejs/vite/issues/2269
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { ReactComponent as ProtoShellLogo } from "@/assets/protoshell.svg";
+import classNames from "classnames/bind";
+import styles from "./CompanyPage.module.scss";
 import { useNavigate, useParams } from "react-router-dom";
 import { getLocks } from "@/supabase/locks";
 import { useAppSelector } from "@/store/hooks";
 
+const cx = classNames.bind(styles);
+
 export const CompanyPage = memo(() => {
   let [locks, setLocks] = useState({});
-
+  const navigate = useNavigate();
   const params = useParams();
 
   const user = useAppSelector((state) => state.auth.user);
@@ -36,6 +39,7 @@ export const CompanyPage = memo(() => {
 
   return (
     <div>
+      <button className={cx("back_button")} onClick={() => navigate(-1)}>{"<-"}</button>
       <h1> company: {params.companyName} </h1>
       <h3> user: {(locks as any).id}</h3>
     </div>

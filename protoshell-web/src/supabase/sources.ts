0f2612a213068ed1090b9bf1d1f2f72544c86d07
jixsu <jacksonxiao0@gmail.com>
2024-02-13 22:11:45 -0500

    remove prints

--- protoshell-web/src/supabase/sources.ts
+++ protoshell-web/src/supabase/sources.ts
@@ -3,12 +3,10 @@ import { handlePostgresResponse, supabaseClient } from ".";
 import { getSourceIdByDBName } from "@/utils/sources";
 
 export const getUserActiveSourceIds = async (userId: string) => {
-  console.log("Getting sources from DB for userid: ", userId);
   const { data } = handlePostgresResponse(
     await supabaseClient.from("sources").select().eq("user_id", userId)
   );
 
-  console.log(data);
   const userSources = data[0];
 
   const keyValPairs = Object.entries(userSources);
